363199b26c921f2f6c55cfb14de46cdb3c54c454
Jose <jocvegar@gmail.com>
2020-11-17 14:50:19 -0600

    connect algolia with firestore

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,42 +10,61 @@
         Add actor {{actorName}} 
       </v-btn>
     </v-row>
-    <v-form>
-      <v-container>
-        <v-row>
-          <v-col cols="8" offset="2">
-            <v-text-field
-              v-model="searchQuery"
-              label="Search"
-              outlined
-              clearable
-            ></v-text-field>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-form>
-    
-    <v-list-item two-line v-for="actor in actors" :key="actor.id">
-      <v-list-item-content>
-        <v-list-item-title>{{actor.name}}</v-list-item-title>
-        <v-list-item-subtitle>{{actor.description}}</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-    
+    <ais-instant-search :search-client="searchClient" index-name="actors">
+        <v-container>
+            <v-row justify="space-between" class="px-3">
+                <v-col cols="12">
+                    <ais-search-box>
+                        <div slot="submit-icon">🔎</div>
+                        <div slot="reset-icon">🚫</div>
+                    </ais-search-box>
+                </v-col>
+            </v-row>
+        </v-container>
+        <v-spacer></v-spacer>
+        <ais-hits>
+          <v-list-item two-line slot="item" slot-scope="{ item }">
+            <v-list-item-content>
+                <v-list-item-title>
+                    <ais-highlight attribute="name" :hit="item" />
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  <ais-highlight attribute="description" :hit="item" />
+                </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </ais-hits>
+
+         <ais-state-results>
+          <template slot-scope="{ state: { query }, results: { hits } }">
+            <!-- show no result if query with no hits -->
+            <div v-if="query && hits.length == 0">
+              <h2 class="text-center mt-5  blue-grey--text text--darken-2">
+                ¡No hay nada!
+              </h2>
+            </div>
+            <div v-else></div>
+          </template>
+        </ais-state-results>
+        <v-spacer></v-spacer>
+    </ais-instant-search>    
   </v-container>
 </template>
 
 <script>
-
 import { firebase, actorsCollection } from "@/firebase"
-
+import algoliasearch from 'algoliasearch/lite';
 export default {
   name: 'HelloWorld',
   data() {
     return {
       searchQuery: "",
       actors: [],
-      actorName: ""
+      actorName: "",
+      searchClient: algoliasearch(
+        process.env.VUE_APP_ALGOLIA_ID,
+        process.env.VUE_APP_ALGOLIA_KEY,
+      ),
     }
   },
   created() {
@@ -71,9 +90,26 @@ export default {
         description: this.$faker().lorem.sentence(),
         name: this.actorName,
       }).then(() => {
-        this.getActors()
+        this.$router.go()
+        this.forceRerender()
       }).catch(error => console.log("error: ", error))
-    }
+    },
   }
 }
 </script>
+
+<style>
+@import url("https://cdn.jsdelivr.net/npm/instantsearch.css@7.3.1/themes/algolia-min.css");
+
+.ais-SearchBox-input {
+  padding: .5em 2.25em;
+}
+.ais-SearchBox-submit {
+  padding-bottom: 1.5rem;
+  padding-left: .5rem;
+}
+.ais-SearchBox-reset {
+  padding-bottom: 1.5rem;
+  padding-right: 1.5rem;
+}
+</style>
