5cc59acf6ff63cd003471b124844296fa5979d52
Jose <jocvegar@gmail.com>
2021-02-15 17:26:34 -0600

    adds favicon

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,8 @@ const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 
 const algoliasearch = require("algoliasearch");
-const APP_ID = "functions.config().algolia.app";
-const ADMIN_KEY = "functions.config().algolia.key";
+const APP_ID = functions.config().algolia.app;
+const ADMIN_KEY = functions.config().algolia.key;
 
 const client = algoliasearch(APP_ID, ADMIN_KEY);
 const index = client.initIndex("actors");
